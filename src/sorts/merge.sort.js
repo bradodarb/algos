69ef7251f419eb8013ef727bbe320f8c741b71aa
brad <bmurry@intraedge.com>
2017-04-22 17:22:07 -0700

    merge sort

--- src/sorts/merge.sort.js
+++ src/sorts/merge.sort.js
@@ -1,32 +1,36 @@
 function merge(arr, left, right) {
 
-    const sizeL = left.length;
-    const sizeR = right.length;
+    const sizeL = left.length -1;
+    const sizeR = right.length -1;
 
-    let i, j = 0;
+    let i = 0;
+    let j = 0;
     let k = 0;
-    while (i < sizeL && j < sizeR) {
-        if (left[i] <= right[j]) {
+
+    while (i <= sizeL && j <= sizeR) {
+        if (left[i] < right[j]) {
             arr[k] = left[i];
-            i++;
+            ++i;
         } else {
             arr[k] = right[j];
-            j++;
+            ++j;
         }
         k++;
     }
 
     //drain
-    while (i < sizeL) {
+    while (i <= sizeL) {
         arr[k] = left[i];
         i++;
         k++;
     }
-    while (j < sizeR) {
+    while (j <= sizeR) {
         arr[k] = right[j];
         j++;
         k++;
     }
+    console.log(`Merge Result::  ${arr.join(',')}`)
+    
 }
 
 function divide(arr) {
@@ -43,22 +47,25 @@ function divide(arr) {
 }
 
 function sort(arr, l, r) {
-    if (l < r) {
-        const m = Math.floor((l + r) / 2);
-        sort(arr, l, m);
-        sort(arr, m + 1, r);
-        merge(arr, l, m, r);
+    if (arr.length > 1 && l < r) {
+        const m = Math.floor((r - l + 1) / 2);
+        const left = arr.slice(0, m);
+        const right = arr.slice(m);
+
+        console.log(`sort left: ${left.join(',')}`);
+        sort(left, 0, left.length - 1);
+        console.log(`sort right: ${right.join(',')}`);
+        sort(right, 0, right.length - 1);
+        console.log(`merge: ${left.join(',')} and ${right.join(',')} on ${arr.join(',')}`);
+        merge(arr, left, right);
     }
 }
 
 function main() {
-    const input = [1, 54, 23, 87, 0, 2, 123, 0, 12, 52, 48, 59, 73, 3];
 
-
-    divide(input);
-
+    const input = [1, 7, 3, 5, 2, 10, 4, 9, 6, 8];
+    sort(input, 0, input.length - 1);
     console.log(`Sorted = ${input.join(',')}`);
-
 }
 
 main();
